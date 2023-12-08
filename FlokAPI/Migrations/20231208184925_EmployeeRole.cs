6ea8a66258687e206d287628ef745ff398ad2a6d
onurkaymak <onurkaymak@outlook.com>
2023-12-08 10:50:09 -0800

    Add new field in ApplicationUser, add new migration.

---
+++ FlokAPI/Migrations/20231208184925_EmployeeRole.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlokAPI.Migrations
+{
+    public partial class EmployeeRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeRole",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeRole",
+                table: "AspNetUsers");
+        }
+    }
+}
