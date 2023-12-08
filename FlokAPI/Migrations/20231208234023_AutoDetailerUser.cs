f60cb1c859223b9d5c97689322b05c4e2f94a064
onurkaymak <onurkaymak@outlook.com>
2023-12-08 15:55:08 -0800

    Add new migration.

---
+++ FlokAPI/Migrations/20231208234023_AutoDetailerUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlokAPI.Migrations
+{
+    public partial class AutoDetailerUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AutoDetailerUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DetailedVehicle = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutoDetailerUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AutoDetailerUsers_AspNetUsers_Id",
+                        column: x => x.Id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AutoDetailerUsers");
+        }
+    }
+}
