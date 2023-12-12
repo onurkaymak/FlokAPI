8989ab35fc58601b1e2b16e729d6292d863227fa
onurkaymak <onurkaymak@outlook.com>
2023-12-12 06:52:12 -0800

    Create Customer Entity.

---
+++ FlokAPI/Models/Customer.cs
@@ -0,0 +1,10 @@
+namespace FlokAPI.Models
+{
+  public class Customer
+  {
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNum { get; set; }
+  }
+}
