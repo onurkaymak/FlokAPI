3a0d635533fa68d84011abf675cff370722f7c10
onurkaymak <onurkaymak@outlook.com>
2023-12-12 06:56:25 -0800

    Add new migration for Customer Entity (DB Seeding).

--- FlokAPI/Migrations/20231212145435_CustomerEntity.Designer.cs
+++ FlokAPI/Migrations/20231212145435_CustomerEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FlokAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FlokAPI.Migrations
 {
     [DbContext(typeof(FlokAPIContext))]
-    partial class FlokAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20231212145435_CustomerEntity")]
+    partial class CustomerEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FlokAPI/Migrations/20231212145435_CustomerEntity.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlokAPI.Migrations
+{
+    public partial class CustomerEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PhoneNum = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Email", "Name", "PhoneNum" },
+                values: new object[,]
+                {
+                    { 1, "carlajohns@gmail.com", "Carla Johns", "618-452-8417" },
+                    { 2, "mcastillo@outlook.com", "Martin Castillo", "860-254-7331" },
+                    { 3, "vincentcooper85@gmail.com", "Vincent Cooper", "841-318-9338" },
+                    { 4, "cras.eget@protonmail.org", "Nora Olsen", "880-547-6442" },
+                    { 5, "primis.in@yahoo.ca", "Sasha Roman", "686-168-5402" },
+                    { 6, "sit.amet@outlook.com", "Erich Booker", "686-107-2226" },
+                    { 7, "amet.consectetuer@protonmail.edu", "Philip Guzman", "231-874-7758" },
+                    { 8, "mauris@google.net", "Tucker Wright", "823-205-3788" },
+                    { 9, "jessmendoza@gmail.com", "Jessica Mendoza", "632-627-4811" },
+                    { 10, "iaculis.nec@outlook.edu", "Kevin Holt", "277-175-1424" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
