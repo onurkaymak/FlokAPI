d8e47f7cdc72de62e08c8e1a3cc6d56ca17f9af8
onurkaymak <onurkaymak@outlook.com>
2023-12-12 07:14:07 -0800

    Add new migration for RentalService Join Entity.

---
+++ FlokAPI/Migrations/20231212151049_RentalService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlokAPI.Migrations
+{
+    public partial class RentalService : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RentalServices",
+                columns: table => new
+                {
+                    RentalServiceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    VehicleId = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    ServiceAgentId = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ReservationStart = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ReservationEnd = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentalServices", x => x.RentalServiceId);
+                    table.ForeignKey(
+                        name: "FK_RentalServices_AspNetUsers_ServiceAgentId",
+                        column: x => x.ServiceAgentId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_RentalServices_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RentalServices_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "VehicleId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentalServices_CustomerId",
+                table: "RentalServices",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentalServices_ServiceAgentId",
+                table: "RentalServices",
+                column: "ServiceAgentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentalServices_VehicleId",
+                table: "RentalServices",
+                column: "VehicleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RentalServices");
+        }
+    }
+}
