2b7b026ef1bd39c5d9e3e8ad2080c51c9e6e543d
onurkaymak <onurkaymak@outlook.com>
2023-12-12 21:48:07 -0800

    Add POST route for RentalController.

--- FlokAPI/Controllers/RentalController.cs
+++ FlokAPI/Controllers/RentalController.cs
@@ -54,4 +54,57 @@ public class RentalController : ControllerBase
   }
 
 
+  [HttpPost]
+  public async Task<ActionResult<RentalService>> Post(RentalServiceDto rentalInfo)
+  {
+    try
+    {
+      Customer customer = await _db.Customers.FirstOrDefaultAsync(c => c.Email == rentalInfo.CustomerEmail);
+      Vehicle vehicle = await _db.Vehicles.FirstOrDefaultAsync(v => v.VIN == rentalInfo.VIN);
+
+      if (vehicle == null || customer == null)
+      {
+        throw new Exception("Vehicle or customer not found, please try again.");
+      }
+
+      if (vehicle.InProduction == true)
+      {
+        throw new Exception("This vehicle is in production, please try again later.");
+      }
+
+#nullable enable
+      RentalService? joinEntity = _db.RentalServices.FirstOrDefault(join => (join.VehicleId == vehicle.VehicleId));
+#nullable disable
+
+      if (joinEntity == null)
+      {
+        _db.RentalServices.Add(new RentalService()
+        {
+          VehicleId = vehicle.VehicleId,
+          CustomerId = customer.CustomerId,
+          ServiceAgentId = rentalInfo.ServiceAgentId,
+          ReservationStart = DateTime.Parse(rentalInfo.ReservationStart, System.Globalization.CultureInfo.InvariantCulture),
+          ReservationEnd = DateTime.Parse(rentalInfo.ReservationEnd, System.Globalization.CultureInfo.InvariantCulture)
+        });
+
+        vehicle.IsRented = true;
+
+        _db.Vehicles.Update(vehicle);
+
+        await _db.SaveChangesAsync();
+      }
+      else
+      {
+        throw new Exception("This vehicle has been rented already.");
+      }
+
+      return Ok(new { status = "success", message = "You successfully booked this vehicle.", vehicle = vehicle, customer = customer, serviceAgent = rentalInfo.ServiceAgentId });
+    }
+    catch (Exception ex)
+    {
+      return BadRequest(new { status = "error", message = ex.Message });
+    }
+  }
+
+
 }
