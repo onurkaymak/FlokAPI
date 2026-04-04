25413d9c12c422c10badacee3e8c33e356cf8bf7
Onur Kaymak <onurkaymak@outlook.com>
2026-04-03 19:58:20 -0700

    Add CreatedAt to DetailingService model

--- FlokAPI/Models/DetailingService.cs
+++ FlokAPI/Models/DetailingService.cs
@@ -1,11 +1,12 @@
 namespace FlokAPI.Models
 {
-  public class DetailingService // Join Entity
+  public class DetailingService
   {
     public int DetailingServiceId { get; set; }
     public int VehicleId { get; set; }
     public Vehicle Vehicle { get; set; } // reference navigation property
-    public string DetailerId { get; set; } // reference navigation property
+    public string DetailerId { get; set; }
     public ApplicationUser Detailer { get; set; } // reference navigation property
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
   }
 }
