b3199754ae0153648b1dcbdd12e8f1e30f049240
Onur Kaymak <onurkaymak@outlook.com>
2026-04-03 19:58:02 -0700

    Remove TotalDetailing from ApplicationUser

--- FlokAPI/Models/ApplicationUser.cs
+++ FlokAPI/Models/ApplicationUser.cs
@@ -5,7 +5,6 @@ namespace FlokAPI.Models
   public class ApplicationUser : IdentityUser
   {
     public string EmployeeRole { get; set; }
-    public int TotalDetailing { get; set; }
     public List<DetailingService> JoinEntities { get; } // collection navigation property.
     public List<RentalService> RentalJoinEntities { get; } // collection navigation property.
   }
